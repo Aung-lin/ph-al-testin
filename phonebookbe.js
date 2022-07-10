c71a42900f489bdd26761e168030ac0dc5274cb0
Thakin Aung Lin <linoscar724@gmail.com>
2022-07-10 21:50:53 +0630

    frontend update

--- phonebookbe.js
+++ phonebookbe.js
@@ -34,6 +34,7 @@ const requestlogger = (request, response, next) => {
 };
 app.use(express.json());
 app.use(requestlogger);
+app.use(express.static("build"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello<h1/>");
